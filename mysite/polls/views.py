6d45a6b479559561d4ce200dcb513cb105f2065e
ParkYoungJun <yachtp@naver.com>
2019-05-09 01:37:00 +0900

    <Part 3> templates/polls 디렉토리를 추가 Django 는 템플릿을 불러오고 렌더링할 수 있음 index.html → views.py 업데이트 (What's up 출력)

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,35 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+    # 템플릿에 context 를 채워넣어 표현한 결과를 HttpResponse 객체와 함께 돌려줌
+    # Django 에서 제공하는 Shortcuts 표현 ↑
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # 404 에러 일으키기
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+    # return HttpResponse("You're looking at question %s." % question_id)
 
 
 def results(request, question_id):
@@ -17,5 +41,5 @@ def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
