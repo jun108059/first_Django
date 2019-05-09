34f94ac35a739c2a2f1311cec646534e55219f5f
ParkYoungJun <yachtp@naver.com>
2019-05-09 22:47:22 +0900

    <Part 5> 뷰를 개선시키고 새로운 뷰 테스트를 확인

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,8 +2,19 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
+
+
+def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
 
 
 class IndexView(generic.ListView):
